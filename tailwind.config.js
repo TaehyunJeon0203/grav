ed35b3a1bf03b76f1635ab3f3cba137bc60fb108
TaehyunJeon <jeontaehyun0203@gmail.com>
2024-08-19 16:47:12 +0900

    Rename: public 폴더 생성 및 파일  이동

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}", "./public/**/*.{html,js}"],
   theme: {
     extend: {},
   },
